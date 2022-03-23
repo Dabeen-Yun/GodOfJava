02579b05e35524a933db1b229c902333a38bbb94
DabeenYun <ekqls09@hanyang.ac.kr>
2022-03-24 01:02:08 +0900

    Add two practices

---
+++ Chap01/HelloGodOfJava.java
@@ -0,0 +1,7 @@
+public class HelloGodOfJava {
+    public static void main(String[] args) {
+        System.out.println("Hello God of Java ~~!!");
+        System.out.print("I want to sleep");
+        System.out.println(", and take a rest.");
+    }
+}
---
+++ Chap01/MyName.java
@@ -0,0 +1,6 @@
+public class MyName {
+    public static void main(String[] args){
+        System.out.println("My name is 윤다빈");
+        System.out.println("My age is 22");
+    }
+}
